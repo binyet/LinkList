f0d492e9852473e650af0170e88db3074be57977
binyet <1244257841@qq.com>
2017-08-14 10:42:10 +0800

    LinkList GetElem

--- ListDemo/ListDemo.cpp
+++ ListDemo/ListDemo.cpp
@@ -79,6 +79,18 @@ LinkList ListReverse(LinkList L) {//非递归方法实现链表反转
 	return L;
 }
 
+LinkList ListReverse2(LinkList L) {//递归实现单链表反转,------最后一个节点被删除---------
+	if (L == NULL || L->next == NULL) {
+		return L;
+	}
+	printf("%d\n", L->data);
+	LinkList newL = ListReverse2(L->next);
+	L->next->next = L;
+	L->next = NULL;
+	return newL;
+
+}
+
 void CreteList(LinkList &L, int n) 
 {//根据用户输入创建单链表
 	L = (LinkList)malloc(sizeof(LNode));
@@ -91,6 +103,51 @@ void CreteList(LinkList &L, int n)
 		p->next = L->next;
 		L->next = p;
 	}
+}
+
+int GetLastNode(LinkList L, int k) {//查找单链表中倒数第i个节点
+	int sum = GetListLength(L);
+	if (k > sum || k<0){
+		printf("倒数第%d个节点不存在", k);
+		return -1;
+	}
+	LinkList newL = L;
+	for (int i = 0; i < k; i++) {//让第一个链表先走k个
+		newL = newL->next;
+	}
+	while (newL != NULL) {//然后两个链表一起走，一直到第一个链表到头，第二个链表少走了k个
+		newL = newL->next;
+		L = L->next;
+	}
+	return L->data;
+
+}
+
+int GetMidNode_1(LinkList L) {//获取单链表的中间(n/2+1)节点
+	if (L == NULL || L->next == NULL)
+		return L->data;
+	LinkList newL = L;
+	while (newL != NULL) {//第一个链表一次走两个，第二个链表一次走一个
+		newL = newL->next;
+		L = L->next;
+		if (newL != NULL)
+			newL = newL->next;
+	}
+	return L->data;
+
+}
+
+int GetMidNode_2(LinkList L) {//获取单链表的中间（n/2+1）节点
+	if (L == NULL || L->next == NULL)
+		return L->data;
+	int num = GetListLength(L);
+	num = num / 2 + 1;
+	while (num--) {//求出单链表长度，直接找到(n/2)+1节点就好
+		L = L->next;
+	}
+	//int data;
+	//GetElem(L, num, data);//直接使用封装的方法
+	return L->data;
 }
 
 
@@ -108,9 +165,9 @@ int main()
 	}*/
 	//CreteList(L, 6);
 	//printf("%d  ", L->next->data);
-	LNode * p = ListReverse(L);
-	display(p);
+	//LNode * p = ListReverse(L);
+	display(L);
 
-	printf("%d  ", GetListLength(p));
+	printf("%d  ", GetMidNode_2(L));
 }
 
